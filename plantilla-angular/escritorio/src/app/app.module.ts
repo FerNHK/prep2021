d19c31809eb6a2da110d9740becb41f98b59e56f
gregoriosl <gregorio_sl@hotmail.com>
2020-12-16 19:57:13 -0500

    Update app.module.ts

--- plantilla-angular/escritorio/src/app/app.module.ts
+++ plantilla-angular/escritorio/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-/*Http Client*/ 
+/*Http Client */ 
 import { HttpClientModule  } from "@angular/common/http";
 
 /*Rutas*/
